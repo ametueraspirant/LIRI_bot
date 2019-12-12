d56ec42117dcdb3c4e6c3a23a21ce639b83b0c50
Amet <ametueraspirant@yahoo.com>
2019-12-11 18:28:35 -0700

    boilerplate set up
    still don't understand why I've done any of this.

---
+++ LIRI.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+// concert_this
+function concert_this()
+{
+
+}
+// spotify_this_song
+function spotify_this_song()
+{
+
+}
+// movie_this
+function movie_this()
+{
+	
+}
+// do_what_it_says
+function do_what_it_says()
+{
+	
+}
+
